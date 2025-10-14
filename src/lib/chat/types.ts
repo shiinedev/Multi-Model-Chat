7a2a64ccad60acd93b70dc8ff2444c8aa11fca8a
shiinedev <shiinecadde17@gmail.com>
2025-10-14 17:31:29 +0100

    added ui message types

---
+++ src/lib/chat/types.ts
@@ -0,0 +1,18 @@
+
+import { tools } from "@/tools/tools";
+import { InferUITools, JSONValue, UIMessage, UIMessagePart } from "ai";
+import z from "zod";
+
+export const metadataSchema = z.object({});
+
+type MyMetadata = z.infer<typeof metadataSchema>;
+
+
+
+export type MyToolSet = InferUITools<typeof tools>;
+
+export type MyUIMessage = UIMessage<MyMetadata, never, MyToolSet>;
+
+export type MyUIMessagePart = UIMessagePart<never, MyToolSet>;
+
+export type MyProviderMetadata = Record<string, Record<string, JSONValue>>;
